3a2541cc92ab59d56cd5644457081a6b76e2c39b
Wang Jun Hong <e0544005@u.nus.edu>
2022-07-11 13:24:03 +0800

    change name

--- src/Header.js
+++ src/Header.js
@@ -14,7 +14,7 @@ function Header({ setGameType }) {
     <div className="header">
       <AppBar style={{ background: "#282c34" }}>
         <div className="logo-name">
-          <div className="logo-anya"></div>Let's Play Up or Down
+          <div className="logo-anya"></div>Let's Play In or Out
         </div>
         <FormGroup>
           <FormControlLabel
